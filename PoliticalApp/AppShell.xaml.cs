b2673538743aa35809d3d0882bb3bd0068442508
Hubert Pilichowski <hpilichowski18@gmail.com>
2025-09-15 16:47:00 -0400

    maui framework basic app structure

---
+++ PoliticalApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PoliticalApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PoliticalApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PoliticalApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PoliticalApp.Pages")]
