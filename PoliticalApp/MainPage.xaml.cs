e908e00f239283f5b2836add1331d0533d34d5c5
Hubert Pilichowski <hpilichowski18@gmail.com>
2025-11-06 15:16:36 -0500

    resolved merged conflicts

--- PoliticalApp/MainPage.xaml.cs
+++ PoliticalApp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿namespace PoliticalApp;
 
+<<<<<<< HEAD
 public partial class MainPage : ContentPage
 {
     public MainPage()
@@ -7,3 +8,31 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 }
+=======
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object? sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	private void OnClickedTwo(object? sender, EventArgs e)
+	{
+		DisplayAlert("Hello", "You clicked the second button!", "OK");
+	}
+}
+>>>>>>> ea6b687 (created Web API folder and descriptions for db)
--- PoliticalApp/obj/Debug/net9.0-ios/iossimulator-arm64/PoliticalApp.AssemblyInfo.cs
+++ PoliticalApp/obj/Debug/net9.0-ios/iossimulator-arm64/PoliticalApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PoliticalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+80f156a1a3b2202dc8e7408eb7abc49e83ee1057")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e8d016ab256235212fd7d61accca01db4a834eb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PoliticalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PoliticalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
